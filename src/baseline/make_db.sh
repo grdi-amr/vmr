c324234d58c0d816f0d383dfd59107b08648608e
ejurga <ejacc@protonmail.com>
2024-07-10 17:54:08 -0400

    adds directory variable

--- src/baseline/make_db.sh
+++ src/baseline/make_db.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 DB=$1
+dir="sql"
 
 # Create the DB
 createdb $DB
 
 # Initial ontology table
-psql --dbname $DB --file ontology_tables.sql
+psql --dbname $DB --file ${dir}/ontology_tables.sql
 
 # Make the ontology lookup tables
 bash make_ontology_lookup_tables.sh | psql --dbname $DB --file=-
 
 # Assemble the main data tables
-psql --dbname $DB --file samples.sql
-psql --dbname $DB --file sample_meta_tables.sql
-psql --dbname $DB --file isolates.sql
-psql --dbname $DB --file seq_bioinf.sql 
-psql --dbname $DB --file ast.sql 
+psql --dbname $DB --file ${dir}/samples.sql
+psql --dbname $DB --file ${dir}/sample_meta_tables.sql
+psql --dbname $DB --file ${dir}/isolates.sql
+psql --dbname $DB --file ${dir}/seq_bioinf.sql 
+psql --dbname $DB --file ${dir}/ast.sql 
 # bioinformatics schema
-psql --dbname $DB --file bioinformatics.sql
-psql --dbname $DB --file misc.sql
+psql --dbname $DB --file ${dir}/bioinformatics.sql
+psql --dbname $DB --file ${dir}/misc.sql
 
 # Input lookup tables 
 
