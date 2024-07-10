895abddd4fea80bcc112758b0aca3194af5f4cb8
ejurga <ejacc@protonmail.com>
2024-07-10 17:58:27 -0400

    adds directory variable

--- bin/dump_lookups.sh
+++ bin/dump_lookups.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DB=$1
+dir="src/baseline/data"
 
 dump_table() {
 	table=$1
@@ -8,19 +9,18 @@ dump_table() {
 		sed '1,19d' | sed -e '/^--/d' | sed  -e '/^$/N;/^\n$/D'
 }
 
-dump_table template_mapping > data/1_mapping.sql
+dump_table template_mapping > ${dir}/1_mapping.sql
 # 
-dump_table ontology_terms > data/2_ontology_terms.sql
+dump_table ontology_terms > ${dir}/2_ontology_terms.sql
 # 
-dump_table host_organisms > data/3_host_organisms.sql
-dump_table countries > data/3_countries.sql
-dump_table microbes > data/3_microbes.sql
-dump_table state_province_regions > data/3_states.sql
+dump_table host_organisms > ${dir}/3_host_organisms.sql
+dump_table countries > ${dir}/3_countries.sql
+dump_table microbes > ${dir}/3_microbes.sql
+dump_table state_province_regions > ${dir}/3_states.sql
 
 mapfile -t lu < <(psql --dbname $DB --command "select table_name from foreign_keys where foreign_table_name = 'ontology_terms'" -P "footer=off" --tuples-only)
 
-
-lu_file=data/4_lookup_tables.sql
+lu_file=${dir}/4_lookup_tables.sql
 echo -n > $lu_file
 for table in ${lu[@]}
 do 
