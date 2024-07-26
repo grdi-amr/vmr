0e3a8d881fdfc04abdfdeb7f1396998676b710c6
ejurga <ejacc@protonmail.com>
2024-07-26 16:21:09 -0400

    prevents errors when view does not exist

--- bin/refresh_views.sh
+++ bin/refresh_views.sh
@@ -3,7 +3,7 @@
 DB=$1
 
 SQL=$(cat <<- 'EOF'
-SELECT 'DROP VIEW ' || table_schema || '.' || table_name || ';' 
+SELECT 'DROP VIEW IF EXISTS ' || table_schema || '.' || table_name || ' CASCADE;' 
    FROM information_schema.views 
   WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
     AND table_name !~ '^pg_';
@@ -12,7 +12,7 @@ EOF
 
 psql --dbname $DB --tuples-only --command "$SQL" | psql --dbname $DB 
 
-for f in src/views/
+for f in src/views/*.sql
 do
 	psql --dbname $DB --file "$f"
 done
