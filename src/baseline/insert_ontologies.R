04d4c1c7fc3c1a07a4fd219a9c5bc1a60f318535
ejurga <ejacc@protonmail.com>
2024-08-09 23:56:26 -0400

    updates to make more portable for updates

--- src/baseline/insert_ontologies.R
+++ src/baseline/insert_ontologies.R
@@ -1,22 +1,107 @@
 library(tidyverse)
 library(DBI)
-library(grdiamR)
 load_all("~/Projects/grdiamR/")
+
+inserts_for_main_ontology <- function(db, ontology_id, en_term){
+
+  message("Adding ", length(ontology_id), " terms")
+  ins <- glue::glue_sql(.con = db, "INSERT INTO ontology_terms (ontology_id, en_term) VALUES ")
+  val <- glue::glue_sql("({ontology_id}, {en_term})", .con = db)
+  sql <- paste0(ins, "\n",paste0(val, collapse = ', \n'), ";")
+
+  return(sql)
+}
+
+inserts_for_host_orgs <- function(df, terms){
+
+  # Organisms.
+  host_org_names <-
+    terms %>%
+    filter(grepl(x=Field, "common") | grepl(x=Field, "scientific")) %>%
+    select(Field, Term, Id) %>%
+    pivot_wider(names_from = Field, values_from = Term, values_fill = NA) %>%
+    janitor::clean_names() %>%
+    rename(ontology_id = id,
+           scientific_name = host_scientific_name,
+           en_common_name = host_common_name)
+
+  dbOrgs <- dbReadTable(vmr, "host_organisms") %>% as_tibble()
+
+  missing <-
+    host_org_names %>%
+    filter(!ontology_id %in% dbOrgs$ontology_id)
+
+  ontology_id <- missing$ontology_id
+  scientific_name <- missing$scientific_name
+  en_common_name <- missing$en_common_name
+
+  if (nrow(missing)>0){
+    message("Adding ", nrow(missing), " terms")
+    ins <- glue::glue_sql(
+      .con = db,
+      "INSERT INTO host_organisms (ontology_id, scientific_name, en_common_name) VALUES")
+    val <- glue::glue_sql("({ontology_id}, {scientific_name}, {en_common_name})", .con = db)
+    sql <- paste0(ins, "\n",paste0(val, collapse = ', \n'), ";")
+    return(sql)
+  }
+}
+
+inserts_for_microbes <- function(db, terms){
+
+  isolate_orgs <-
+    terms %>%
+    filter(Field=="organism") %>%
+    rename(ontology_id = Id,
+           scientific_name = Term) %>%
+    select(-Field, -version) %>%
+    filter(scientific_name != "Streptococcus equinus")
+
+  dbIsoOrgs <- dbReadTable(db, name = "microbes") %>% as_tibble()
+
+  missing <-
+    isolate_orgs %>%
+    filter(!ontology_id %in% dbIsoOrgs$ontology_id)
+
+  ontology_id <- missing$ontology_id
+  scientific_name <- missing$scientific_name
+
+  if (nrow(missing)>0){
+    message("Adding ", nrow(missing), " terms")
+    ins <- glue::glue_sql(.con = db, "INSERT INTO microbes (ontology_id, scientific_name) VALUES ")
+    val <- glue::glue_sql("({ontology_id}, {scientific_name})", .con = db)
+    sql <- paste0(ins, "\n",paste0(val, collapse = ', \n'), ";")
+    return(sql)
+  }
+}
+
+insert_into_lookup_table <- function(db, vmr_table, lookup_terms){
+
+  q <- glue::glue_sql(.con = db,
+  "SELECT ontology_id FROM {`vmr_table`} as t LEFT JOIN ontology_terms AS o ON t.ontology_term_id = o.id")
+  db_lookups <- dbGetQuery(vmr, q)
+
+  missing <- lookup_terms[!lookup_terms %in% db_lookups$ontology_id]
+
+  if (length(missing > 0)){
+    message("New values for lookup table ", vmr_table)
+    ins_lu_sql <-
+      glue::glue_sql(.con = db,
+      "-- Update lookup table {`vmr_table`}
+      WITH lookup AS
+      (SELECT id
+         FROM ontology_terms
+        WHERE ontology_id IN
+      ({missing*}))
+      INSERT INTO {`vmr_table`} (ontology_term_id)
+      SELECT id FROM lookup;")
+    return(ins_lu_sql)
+  }
+
+}
 
 template_file <- "~/Software/GRDI_AMR_One_Health/Template/GRDI_Harmonization-Template_v12.2.2.xlsm"
 
 excel_vals <- excel_lookup_values(template_file = template_file)
-
-populate_lookup_table <- function(con, lookup_table, terms){
-  message("Table: ", lookup_table)
-  query_string <-
-    glue::glue_sql(
-      .con = con,
-      "INSERT into ", lookup_table,
-      " (ontology_term_id) VALUES ( (SELECT id FROM ontology_terms WHERE ontology_id = $1) )
-      ON CONFLICT (ontology_term_id) DO NOTHING")
-  dbExecute(con, query_string, params = list(terms))
-}
 
 vmr <-
   dbConnect(drv = RPostgres::Postgres(),
@@ -36,7 +121,8 @@ excel_fields <- colnames(sheet)
 xcel_no_amr <- excel_fields[!grepl(x=excel_fields, paste(grdiamR::amr_regexes(), collapse = "|"))]
 
 missing_fields_in_vmr <- xcel_no_amr[!xcel_no_amr %in% map$grdi_field]
-
+message("These fields appear to be missing in the vmr. Do they need to be added?\n",
+        paste0(missing_fields_in_vmr, collapse = ', '))
 
 all_vmr_cols <- dbGetQuery(vmr, "SELECT * FROM information_schema.columns")
 map %>% filter(!vmr_table %in% all_vmr_cols$table_name)
@@ -47,89 +133,8 @@ terms <-
   filter(!is.na(Id)) %>%
   arrange(Field)
 
-# Countries and Statess
-countries <-
-  terms %>%
-  filter(grepl(x=Field, "country")) %>%
-  select(Term, Id) %>%
-  rename(ontology_id = Id, en_term = Term) %>%
-  distinct() %>%
-  arrange(ontology_id) %>%
-  filter(!en_term == "Swaziland")
-
-dbCountries <- dbReadTable(vmr, "countries") %>% as_tibble()
-
-countries_to_add <-
-  countries %>%
-  filter(!ontology_id %in% dbCountries$ontology_id)
-
-if (nrow(countries_to_add)>0){
-  dbAppendTable(vmr, name = "countries", value = countries_to_add)
-}
-
-canada_id <-
-  dbSendQuery(vmr, "SELECT id FROM countries WHERE en_term = 'Canada'") %>%
-  dbFetch() %>% pull(id)
-states <-
-  terms %>%
-  filter(grepl(x=Field, "state")) %>%
-  select(Term, Id) %>%
-  rename(ontology_id = Id, en_term = Term) %>%
-  arrange(ontology_id) %>%
-  filter(!grepl(x=ontology_id, "GENEPIO")) %>%
-  mutate(country_id = canada_id)
-
-dbStates <- dbReadTable(vmr, "state_province_regions") %>% as_tibble()
-
-states_to_add <-
-  states %>%
-  filter(!ontology_id %in% dbStates$ontology_id)
-
-if (nrow(states_to_add)>0){
-  dbAppendTable(vmr, name = "state_province_regions", value = states_to_add)
-}
-
-# Organisms.
-host_org_names <-
-  terms %>%
-  filter(grepl(x=Field, "common") | grepl(x=Field, "scientific")) %>%
-  select(Field, Term, Id) %>%
-  pivot_wider(names_from = Field, values_from = Term, values_fill = NA) %>%
-  janitor::clean_names() %>%
-  rename(ontology_id = id,
-         scientific_name = host_scientific_name,
-         en_common_name = host_common_name)
-
-dbOrgs <- dbReadTable(vmr, "host_organisms") %>% as_tibble()
-
-host_orgs_to_add <-
-  host_org_names %>%
-  filter(!ontology_id %in% dbOrgs$ontology_id)
-
-if (nrow(host_orgs_to_add)>0){
-  dbAppendTable(vmr, name = "host_organisms", value = host_orgs_to_add)
-}
-
-
-isolate_orgs <-
-  terms %>%
-  filter(Field=="organism") %>%
-  rename(ontology_id = Id,
-         scientific_name = Term) %>%
-  select(-Field, -version) %>%
-  filter(scientific_name != "Streptococcus equinus")
-
-dbIsoOrgs <- dbReadTable(vmr, name = "microbes") %>% as_tibble()
-
-isos_to_add <-
-  isolate_orgs %>%
-  filter(!ontology_id %in% dbIsoOrgs$ontology_id)
-
-if (nrow(isos_to_add)>0){
-  message("Adding ", nrow(isos_to_add), " terms")
-  message(paste(isos_to_add$scientific_name, collapse = "; "))
-  dbAppendTable(vmr, name = "microbes", value = isos_to_add)
-}
+microbes_sql <- inserts_for_microbes(vmr, terms)
+host_orgs_sql <- inserts_for_host_orgs(vmr, terms)
 
 map_to_tables <-
   map %>%
@@ -140,8 +145,13 @@ map_to_tables <-
 terms_insert <-
   terms %>%
   mutate(Field = sub(x=Field, " menu$", ""))
-x <- terms_insert$Field=="antimicrobial_phenotype"
-terms_insert$Field[x] <- "antimicrobial_resistance_phenotype"
+terms_insert <-
+  terms_insert %>%
+  mutate(Field = case_when(
+    Field=="antimicrobial_phenotype" ~ "antimicrobial_resistance_phenotype",
+    Field=="quality_control_determination" ~ "quality control determination",
+    Field=="quality_control_issues" ~ "quality control issues",
+    TRUE ~ Field))
 
 tab_fk_to_ont_term <- fk %>% filter(foreign_table_name=='ontology_terms')
 terms_to_ins <-
@@ -150,29 +160,42 @@ terms_to_ins <-
   filter(!is.na(foreign_table_name)) %>%
   filter(foreign_table_name %in% tab_fk_to_ont_term$table_name) %>%
   left_join(terms_insert, by = c("grdi_field"="Field")) %>%
-  group_by(foreign_table_name) %>%
-  summarise(ontology_terms = list(Id)) %>%
-  rowwise() %>%
-  mutate(ontology_terms = list(unique(ontology_terms))) %>%
-  filter(!foreign_table_name %in% c("prevalence_metrics", "stage_of_production"))
+  select(grdi_field, foreign_table_name, Term, Id) %>%
+  filter(!foreign_table_name %in% c("prevalence_metrics", "stage_of_production")) %>%
+  distinct()
 
 unique_terms <-
-  terms_insert %>%
-  filter(Id %in% unlist(terms_to_ins$ontology_terms)) %>%
+  terms_to_ins %>%
   select(Term, Id) %>%
   distinct() %>%
   filter(!duplicated(Id)) %>%
   rename(ontology_id = Id, en_term = Term)
-dbAppendTable(vmr, name = "ontology_terms", value = unique_terms)
 
-mapply(populate_lookup_table,
-       lookup_table = terms_to_ins$foreign_table_name,
-       terms = terms_to_ins$ontology_terms,
-       MoreArgs = list(con = vmr))
+dbOntTerms <- dbReadTable(vmr, "ontology_terms") %>% as_tibble()
 
+ontology_terms_to_add <-
+  unique_terms %>%
+  filter(!ontology_id %in% dbOntTerms$ontology_id)
 
+main_ontology_ins <-
+  inserts_for_main_ontology(vmr, ontology_terms_to_add$ontology_id, ontology_terms_to_add$en_term)
 
+lookups <-
+  terms_to_ins %>%
+  group_by(foreign_table_name) %>%
+  summarise(terms = list(unique(Id)))
 
+lus <- mapply(insert_into_lookup_table,
+              vmr_table = lookups$foreign_table_name,
+              lookups$terms,
+              MoreArgs = list(db=vmr))
 
+sql_full <-
+  paste(
+    main_ontology_ins,
+    microbes_sql,
+    host_orgs_sql,
+    sep = "\n\n",
+    paste0(unlist(lus), collapse = "\n\n"))
 
-
+write_file(sql_full, file = "test.sql")
