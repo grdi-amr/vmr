c4c6f0094960f3c8d5cdc7368410f006723dd43f
ejurga <ejacc@protonmail.com>
2024-08-15 15:22:35 -0400

    now refreshes functions as well

--- bin/refresh_views.sh
+++ bin/refresh_views.sh
@@ -2,7 +2,16 @@
 
 DB=$1
 
-SQL=$(cat <<- 'EOF'
+# A command to get all functions defined in public
+FUNCS=$(cat <<- 'EOF'
+SELECT 'DROP FUNCTION IF EXISTS public.' || routine_name || ';' 
+  FROM information_schema.routines 
+ WHERE routine_type = 'FUNCTION' AND routine_schema = 'public';
+EOF
+)
+
+# A command to get all views
+VIEWS=$(cat <<- 'EOF'
 SELECT 'DROP VIEW IF EXISTS ' || table_schema || '.' || table_name || ' CASCADE;' 
    FROM information_schema.views 
   WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
@@ -10,9 +19,20 @@ SELECT 'DROP VIEW IF EXISTS ' || table_schema || '.' || table_name || ' CASCADE;
 EOF
 )
 
-psql --dbname $DB --tuples-only --command "$SQL" | psql --dbname $DB 
+# Drop Views
+psql --quiet --dbname $DB --tuples-only --command "$VIEWS" | psql --quiet --dbname $DB
+# Drop Functions
+psql --quiet --dbname $DB --tuples-only --command "$FUNCS" | psql --quiet --dbname $DB 
 
-for f in src/views/*.sql
+# Refresh functions
+for func in src/functions/*.sql
 do
-	psql --dbname $DB --file "$f"
+  psql --dbname $DB --file "$func"
 done
+
+# Refresh views
+for view in src/views/*.sql
+do
+  psql --dbname $DB --file "$view"
+done
+
