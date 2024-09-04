690c8762dcdeb54e258eabc077f3d91efdb588cd
ejurga <ejacc@protonmail.com>
2024-09-04 18:25:02 -0400

    adds number of AST tests

--- src/views/5_pbi_exports.sql
+++ src/views/5_pbi_exports.sql
@@ -1,4 +1,4 @@
-CREATE VIEW project_stats
+CREATE OR REPLACE VIEW project_stats
 AS
 SELECT p.id AS database_id, 
        p.sample_plan_id AS "Sample plan ID",
@@ -6,7 +6,8 @@ SELECT p.id AS database_id,
        project_name AS "Project name",
        Count(DISTINCT s.id) AS "Number of Samples",
        Count(DISTINCT i.id) AS "Number of Isolates",
-       Count(DISTINCT wgs.sequencing_id) AS "Number of WGS" 
+       Count(DISTINCT wgs.sequencing_id) AS "Number of WGS",
+       Count(DISTINCT ab.id) AS "Number of AST"
   FROM projects AS p
        LEFT JOIN samples AS s
 	      ON p.id = s.project_id
@@ -14,4 +15,6 @@ SELECT p.id AS database_id,
 	      ON i.sample_id = s.id
        LEFT JOIN wgs 
 	      ON wgs.isolate_id = i.id
+       LEFT JOIN am_susceptibility_tests as ab
+	      ON ab.isolate_id =  i.id
 GROUP BY p.id;
