e11903f79fc7668715b2c8ffaf77b9c59db29fa9
ejurga <ejacc@protonmail.com>
2024-10-01 18:13:32 -0400

    adds location column to pbi.drugs view

--- src/views/5_pbi_exports.sql
+++ src/views/5_pbi_exports.sql
@@ -107,7 +107,8 @@ SELECT arg.project_name,
        arg.organism,
        arg.library_id, 
        arg.amr_genes_id,
-       best_hit_aro,
+       arg.region AS province,
+       arg.best_hit_aro,
        arg.cut_off,
        drugs.drug_id
 FROM pbi.arg AS arg
