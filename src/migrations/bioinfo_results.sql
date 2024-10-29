e7dbf79a9c2f161264942b992470f27201cda60d
ejurga <ejacc@protonmail.com>
2024-10-29 14:24:10 -0400

    Removes prepended res_ to names

--- src/migrations/bioinfo_results.sql
+++ src/migrations/bioinfo_results.sql
@@ -1,19 +1,19 @@
 --Resfinder results - One sequencing can get multiple genes results which can have many predicted phenotypes each gene
-CREATE TABLE bioinf.res_resfinder (
+CREATE TABLE bioinf.resfinder (
     sequencing_id int4 REFERENCES sequencing(id),
     resfinder_gene text,
-    CONSTRAINT res_resfinderpkey PRIMARY KEY (sequencing_id, resfinder_gene)
+    CONSTRAINT resfinderpkey PRIMARY KEY (sequencing_id, resfinder_gene)
 );
 
 CREATE TABLE bioinf.resfinder_predicted_phenotypes (
-    sequencing_id int4 REFERENCES bioinf.res_resfinder(sequencing_id),
-    resfinder_gene text REFERENCES bioinf.res_resfinder(resfinder_gene),
+    sequencing_id int4 REFERENCES bioinf.resfinder(sequencing_id),
+    resfinder_gene text REFERENCES bioinf.resfinder(resfinder_gene),
     predicted_phenotype text,
     CONSTRAINT resfinder_predicted_phenotypes_pkey PRIMARY KEY (sequencing_id, resfinder_gene, predicted_phenotype)
 );
 
 -- MLST results, one sequencing has one MLST sequence
-CREATE TABLE bioinf.res_mlst (
+CREATE TABLE bioinf.mlst (
     id int4 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     sequencing_id int4 REFERENCES sequencing(id),
     mlst_sequence text,
@@ -21,14 +21,14 @@ CREATE TABLE bioinf.res_mlst (
 );
 
 -- Plasmid finder results , each sequencing can have multiple plasmids
-CREATE TABLE bioinf.res_plasmid_finder (
+CREATE TABLE bioinf.plasmid_finder (
     sequencing_id int4 REFERENCES sequencing(id),
     plasmid text,
-    CONSTRAINT res_plasmid_finder PRIMARY KEY (sequencing_id, plasmid)
+    CONSTRAINT plasmid_finder PRIMARY KEY (sequencing_id, plasmid)
 );
 
 --Serotyping for e.coli -> one serotype for each sequencing
-CREATE TABLE bioinf.res_ecoli_serotyping (
+CREATE TABLE bioinf.ecoli_serotyping (
     id int4 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     sequencing_id int4 REFERENCES sequencing(id),
     ecoli_serotype text,
@@ -37,7 +37,7 @@ CREATE TABLE bioinf.res_ecoli_serotyping (
 );
 
 -- Salmonella serotyping, one serotype for each sequencing
-CREATE TABLE bioinf.res_salmonella_serotyping  (
+CREATE TABLE bioinf.salmonella_serotyping  (
     id int4 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     sequencing_id int4 REFERENCES sequencing(id),
     serovar text,
@@ -50,17 +50,17 @@ CREATE TABLE bioinf.res_salmonella_serotyping  (
 );
 
 --VFDB using Abricate --> Multiple genes for each sequencing.
-CREATE TABLE bioinf.res_virulence_VFDB (
+CREATE TABLE bioinf.virulence_VFDB (
     sequencing_id int4 REFERENCES sequencing(id),
     vfdb_gene_accession text,
     vfdb_product_resisence text
-    CONSTRAINT res_virulence_VFDB_pkey PRIMARY KEY (sequencing_id, vfdb_gene_accession)
+    CONSTRAINT virulence_VFDB_pkey PRIMARY KEY (sequencing_id, vfdb_gene_accession)
 );
 
 -- Virulence finder results
-CREATE TABLE bioinf.res_virulence_VF (
+CREATE TABLE bioinf.virulence_VF (
     sequencing_id int4 REFERENCES sequencing(id),
     vf_gene text,
     vf_protein_function text
-    CONSTRAINT res_virulence_VF_pkey PRIMARY KEY (sequencing_id, vf_gene)
+    CONSTRAINT virulence_VF_pkey PRIMARY KEY (sequencing_id, vf_gene)
 );
