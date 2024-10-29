a77f6f64dd21e8fc88221b167d954e314263ef1e
ejurga <ejacc@protonmail.com>
2024-10-29 14:35:11 -0400

    adjusts ids and references

--- src/migrations/bioinfo_results.sql
+++ src/migrations/bioinfo_results.sql
@@ -1,15 +1,14 @@
 --Resfinder results - One sequencing can get multiple genes results which can have many predicted phenotypes each gene
 CREATE TABLE bioinf.resfinder (
+    id int4 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     sequencing_id int4 REFERENCES sequencing(id),
     resfinder_gene text,
-    CONSTRAINT resfinderpkey PRIMARY KEY (sequencing_id, resfinder_gene)
 );
 
 CREATE TABLE bioinf.resfinder_predicted_phenotypes (
-    sequencing_id int4 REFERENCES bioinf.resfinder(sequencing_id),
-    resfinder_gene text REFERENCES bioinf.resfinder(resfinder_gene),
+    resfinder_id int4 REFERENCES bioinf.resfinder(id),
     predicted_phenotype text,
-    CONSTRAINT resfinder_predicted_phenotypes_pkey PRIMARY KEY (sequencing_id, resfinder_gene, predicted_phenotype)
+    CONSTRAINT resfinder_predicted_phenotypes_pkey PRIMARY KEY (resfinder_id, predicted_phenotype)
 );
 
 -- MLST results, one sequencing has one MLST sequence
@@ -22,9 +21,9 @@ CREATE TABLE bioinf.mlst (
 
 -- Plasmid finder results , each sequencing can have multiple plasmids
 CREATE TABLE bioinf.plasmid_finder (
+    id int4 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     sequencing_id int4 REFERENCES sequencing(id),
-    plasmid text,
-    CONSTRAINT plasmid_finder PRIMARY KEY (sequencing_id, plasmid)
+    plasmid text
 );
 
 --Serotyping for e.coli -> one serotype for each sequencing
@@ -51,16 +50,16 @@ CREATE TABLE bioinf.salmonella_serotyping  (
 
 --VFDB using Abricate --> Multiple genes for each sequencing.
 CREATE TABLE bioinf.virulence_VFDB (
+    id PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     sequencing_id int4 REFERENCES sequencing(id),
     vfdb_gene_accession text,
     vfdb_product_resisence text
-    CONSTRAINT virulence_VFDB_pkey PRIMARY KEY (sequencing_id, vfdb_gene_accession)
 );
 
 -- Virulence finder results
 CREATE TABLE bioinf.virulence_VF (
-    sequencing_id int4 REFERENCES sequencing(id),
+    virulance_VFDB_id int4 REFERENCES (bioinf.virulence_VFDB(id),	
     vf_gene text,
     vf_protein_function text
-    CONSTRAINT virulence_VF_pkey PRIMARY KEY (sequencing_id, vf_gene)
+    CONSTRAINT virulence_VF_pkey PRIMARY KEY (virulance_VFDB_id, vf_gene)
 );
