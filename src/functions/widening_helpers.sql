c6786fe76f50f8b2e5b17ef1b16c7fb588078ef0
ejurga <ejacc@protonmail.com>
2024-12-03 14:41:07 -0500

    adds function to aggregate values in multi-choice tables

---
+++ src/functions/widening_helpers.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION aggregate_multi_choice_table(table_name text) 
+   RETURNS TABLE (id integer, vals text) 
+AS $$
+BEGIN
+   RETURN QUERY EXECUTE format('
+     SELECT id,
+  	    string_agg(ontology_full_term(term_id), ''; '') AS terms
+       FROM %I 
+   GROUP BY id', table_name);
+END; 
+$$ LANGUAGE plpgsql;
