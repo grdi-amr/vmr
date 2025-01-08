5652fecb8078a37e8429d4f487cd593dca5d10e2
Emil Jurga <38766486+ejurga@users.noreply.github.com>
2025-01-08 16:19:53 -0500

    17 update to 13.4.4 (#21)
    Updates schema to conform to v13.4.4 of the data standard. 
    
    * Init
    
    * adds new columns to views
    
    * linting
    
    * adds placeholder data types
    
    * adds new colums to the views
    
    * fixes some errors
    
    * lints the fields
    
    * fixes error in column
    
    * removes whitespace and drop statememt
    
    * adds missing comma
    
    * changes type to int
    
    * adds and renames tables for environmental_data fields
    
    * adds new weather cols
    
    * Adds new weather cols, and adjusts formatting for better versioning
    
    * Removes trailing whitespace

--- src/views/2_metadata_widening.sql
+++ src/views/2_metadata_widening.sql
@@ -3,21 +3,29 @@ CREATE OR REPLACE VIEW collection_information_wide
 AS
 SELECT col.sample_id,
        col.original_sample_description,
-       ontology_full_term(col.sample_collected_by)              AS sample_collected_by,
-       contacts.contact_name                                    AS sample_collector_contact_name,
-       contacts.contact_email                                   AS sample_collector_contact_email,
-       contacts.laboratory_name                                 AS sample_collector_laboratory_name,
+       ontology_full_term(col.sample_collected_by)                  AS sample_collected_by,
+       contacts.contact_name                                        AS sample_collector_contact_name,
+       contacts.contact_email                                       AS sample_collector_contact_email,
+       contacts.laboratory_name                                     AS sample_collector_laboratory_name,
        col.sample_collection_date,
-       ontology_full_term(col.sample_collection_date_precision) AS sample_collection_date_precision,
-       activities.vals                                          AS presamping_activity,
+       col.sample_collection_end_date,
+       col.sample_collection_start_time,
+       col.sample_collection_end_time,
+       ontology_full_term(col.sample_collection_time_of_day)        AS sample_collection_time_of_day,
+       col.sample_collection_time_duration_value,
+       ontology_full_term(col.sample_collection_time_duration_unit) AS sample_collection_time_duration_unit,
+       ontology_full_term(col.sample_collection_date_precision)     AS sample_collection_date_precision,
+       activities.vals                                              AS presamping_activity,
        col.presampling_activity_details,
-       purposes.vals                                            AS purpose_of_samping,
+       purposes.vals                                                AS purpose_of_samping,
        col.sample_received_date,
-       ontology_full_term(col.specimen_processing)              AS specimen_processing,
+       ontology_full_term(col.specimen_processing)                  AS specimen_processing,
+       col.specimen_processing_details,
        col.sample_storage_method,
        col.sample_storage_medium,
-       ontology_full_term(col.collection_device)                AS collection_device,
-       ontology_full_term(col.collection_method)                AS collection_method
+       ontology_full_term(col.collection_device)                    AS collection_device,
+       ontology_full_term(col.collection_method)                    AS collection_method,
+       col.sample_processing_date
   FROM collection_information AS col
        LEFT JOIN contact_information AS contacts ON contacts.id = col.contact_information
        LEFT JOIN (SELECT id,vals FROM aggregate_multi_choice_table('sample_activity')) AS activities
@@ -92,22 +100,26 @@ WITH mat_const AS (
     FROM environmental_data_material_constituents
 GROUP BY id
 )
-SELECT env.sample_id                                    AS sample_id,
+SELECT env.sample_id                                      AS sample_id,
        env.air_temperature,
-       ontology_full_term(env.air_temperature_units)    AS air_temperature_units,
+       ontology_full_term(env.air_temperature_units)      AS air_temperature_units,
        env.water_depth,
-       ontology_full_term(env.water_depth_units)        AS water_depth_units,
+       ontology_full_term(env.water_depth_units)          AS water_depth_units,
        env.water_temperature,
-       ontology_full_term(env.water_temperature_units)  AS water_temperature_units,
+       ontology_full_term(env.water_temperature_units)    AS water_temperature_units,
        env.sediment_depth,
-       ontology_full_term(env.sediment_depth_units)     AS sediment_depth_units,
+       ontology_full_term(env.sediment_depth_units)       AS sediment_depth_units,
        env.available_data_type_details,
-       avail_type.vals                                  AS available_data_types,
-       animal_or_plant_pops.vals                        AS animal_or_plant_population,
-       mat.vals                                         AS environmental_materials,
-       mat_const.vals                                   AS environmental_material_constituent,
-       site.vals                                        AS environmental_sites,
-       weather.vals                                     AS weather_type
+       avail_type.vals                                    AS available_data_types,
+       animal_or_plant_pops.vals                          AS animal_or_plant_population,
+       mat.vals                                           AS environmental_materials,
+       mat_const.vals                                     AS environmental_material_constituent,
+       site.vals                                          AS environmental_sites,
+       sample_weather.vals                                AS sampling_weather_conditions,
+       presample_weather.vals                             AS presampling_weather_conditions,
+       precipitation_measurement_value,
+       ontology_full_term(precipitation_measurement_unit) AS precipitation_measurement_unit,
+       precipitation_measurement_method
   FROM environmental_data AS env
        LEFT JOIN (SELECT id,vals FROM aggregate_multi_choice_table('environmental_data_animal_plant')) AS animal_or_plant_pops
               ON animal_or_plant_pops.id = env.id
@@ -117,6 +129,10 @@ SELECT env.sample_id                                    AS sample_id,
               ON mat.id = env.id
        LEFT JOIN (SELECT id,vals FROM aggregate_multi_choice_table('environmental_data_site')) AS site
               ON site.id = env.id
+       LEFT JOIN (SELECT id,vals FROM aggregate_multi_choice_table('environmental_data_sampling_weather_conditions')) AS sample_weather
+              ON sample_weather.id = env.id
+       LEFT JOIN (SELECT id,vals FROM aggregate_multi_choice_table('environmental_data_presampling_weather_conditions')) AS presample_weather
+              ON presample_weather.id = env.id
        LEFT JOIN (SELECT id,vals FROM aggregate_multi_choice_table('environmental_data_weather_type')) AS weather
               ON weather.id = env.id
        LEFT JOIN mat_const ON mat_const.id = env.id;
@@ -212,7 +228,7 @@ SELECT wgs.isolate_id                                             AS isolate_id,
        wgs.r1_fastq_filename,
        wgs.r2_fastq_filename,
        wgs.fast5_filename,
-       wgs.assembly_filename,
+       wgs.genome_sequence_filename,
        wgs.r1_irida_id,
        wgs.r2_irida_id
   FROM wgs
