5f34b9a0388553ad1a3df6ff1e77e1cb8203504a
ejurga <ejacc@protonmail.com>
2025-02-05 19:49:48 -0500

    Adds versioning, and dyn SQL func

--- src/migrations/v1.11.sql
+++ src/migrations/v1.11.sql
@@ -1,8 +1,10 @@
+INSERT INTO db_versions (major_release, minor_release, script_name, grdi_template_version, date_applied)
+   VALUES (1,11,'v1.11.sql', 'v14.4.4', CURRENT_DATE);
+
 CREATE EXTENSION IF NOT EXISTS hstore;
 
 CREATE SCHEMA audit;
 REVOKE ALL ON SCHEMA audit FROM public;
-
 COMMENT ON SCHEMA audit IS 'Out-of-table audit/history logging tables and trigger functions';
 
 DROP TABLE if exists audit.logged_actions;
@@ -19,7 +21,24 @@ CREATE TABLE audit.logged_actions (
   previous_values   hstore
 );
 
-
+DO $$
+DECLARE
+    table_name text;
+BEGIN
+    FOR table_name IN
+	(SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_name != 'db_versions' AND table_type = 'BASE TABLE')
+    LOOP
+        EXECUTE format(
+	 'ALTER TABLE public.%I
+	        DROP COLUMN updated_at,
+	        DROP COLUMN updated_by,
+          ADD  COLUMN was_updated bool DEFAULT FALSE;
+	  CREATE TRIGGER audit_changes_to_ext_table
+	         BEFORE UPDATE ON public.%I
+                 FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func()', table_name,table_name);
+    END loop;
+END;
+$$ language 'plpgsql';
 DROP TRIGGER audit ON projects;
 CREATE TRIGGER audit BEFORE UPDATE OR DELETE on projects FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
 
