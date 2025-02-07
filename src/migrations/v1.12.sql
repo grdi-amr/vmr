8b59a1f81455c39f05236c2ee75c5a84903320eb
ejurga <ejacc@protonmail.com>
2025-02-07 16:24:28 -0500

    Finishes dyn SQL to mass-update DB with trigger

--- src/migrations/v1.12.sql
+++ src/migrations/v1.12.sql
@@ -3,20 +3,47 @@ INSERT INTO db_versions (major_release, minor_release, script_name, grdi_templat
 
 DO $$
 DECLARE
-    tablename text;
+    table_row record;
 BEGIN
-    FOR tablename IN
-	(SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_name != 'db_versions' AND table_type = 'BASE TABLE')
+    FOR table_row IN
+	(WITH public_cols AS (
+		SELECT table_name, column_name, data_type 
+		  FROM information_schema.columns
+		 WHERE table_schema = 'public'
+		   AND table_name   NOT IN (SELECT table_name FROM information_schema.views)
+	),
+	tables_with_id AS (
+		SELECT table_name
+		  FROM public_cols
+		 WHERE column_name = 'id'
+	),
+	tables_with_dif_id AS (
+		SELECT table_name, column_name
+		  FROM public_cols
+	   WHERE table_name  NOT IN (SELECT table_name FROM tables_with_id)
+	   	 AND column_name LIKE '%id'
+		 AND data_type   = 'integer'
+	),
+	table_and_id_col AS (
+		SELECT table_name, NULL AS column_name 
+		  FROM tables_with_id
+		 UNION
+		SELECT table_name, column_name
+	    FROM tables_with_dif_id 
+		 WHERE table_name  != 'wgs_extractions'
+		   AND column_name != 'extraction_id'
+	)
+	SELECT table_name, column_name from table_and_id_col)
     LOOP
         EXECUTE format(
 	 'ALTER TABLE public.%I
-	        DROP COLUMN updated_at,
+	        DROP COLUMN IF EXISTS updated_at,
 	        DROP COLUMN updated_by,
-          ADD  COLUMN was_updated bool DEFAULT FALSE;
-	  DROP trigger update_usertimestamp ON public.%I;
+		ADD  COLUMN was_updated bool DEFAULT FALSE;
+	  DROP TRIGGER update_usertimestamp ON public.%I;
 	  CREATE TRIGGER audit_changes_to_ext_table
 	         BEFORE UPDATE OR DELETE ON public.%I
-                 FOR EACH ROW EXECUTE PROCEDURE audit.log_changes()', tablename,tablename,tablename);
-    END loop;
+                 FOR EACH ROW EXECUTE PROCEDURE audit.log_changes(%s)', table_row.table_name,table_row.table_name,table_row.table_name,table_row.column_name);
+    END LOOP;
 END;
 $$ language 'plpgsql';
