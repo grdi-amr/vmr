78243ffed0b540e991e61ffd8b19a749085d5b15
ejurga <ejacc@protonmail.com>
2025-05-21 15:14:14 -0400

    removes function replace

--- bin/refresh_views.sh
+++ bin/refresh_views.sh
@@ -4,14 +4,6 @@ read -s PGPASSWORD
 echo \n
 export PGPASSWORD
 
-# A command to get all functions defined in public
-FUNCS=$(cat <<- 'EOF'
-SELECT 'DROP FUNCTION IF EXISTS public.' || routine_name || ';' 
-  FROM information_schema.routines 
- WHERE routine_type = 'FUNCTION' AND routine_schema = 'public';
-EOF
-)
-
 # A command to get all views
 VIEWS=$(cat <<- 'EOF'
 SELECT 'DROP VIEW IF EXISTS ' || table_schema || '.' || table_name || ' CASCADE;' 
@@ -23,14 +15,6 @@ EOF
 
 # Drop Views
 psql "$@" --quiet --tuples-only --command "$VIEWS" | psql "$@" --quiet
-# Drop Functions
-psql "$@" --quiet --tuples-only --command "$FUNCS" | psql "$@" --quiet
-
-# Refresh functions
-for func in src/functions/*.sql
-do
-  psql "$@" --file "$func"
-done
 
 # Refresh views
 for view in src/views/*.sql
