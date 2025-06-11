c1d4d093db71837b9019daa4e2e986847382ba9c
ejurga <ejacc@protonmail.com>
2025-06-11 14:25:45 -0400

    Adds version control entry, and linting

--- src/migrations/v2.04.sql
+++ src/migrations/v2.04.sql
@@ -1,9 +1,8 @@
---fixing some bioinf.kleborate columns
+INSERT INTO db_versions (major_release, minor_release, script_name, grdi_template_version, date_applied, note)
+   VALUES (2,4,'v2.04.sql', 'v14.5.4', CURRENT_DATE, 'Fixes some tables in the bioinf schema');
 
-ALTER TABLE bioinf.kleborate
-RENAME COLUMN spurious_virulence_hits TO spurious_resistance_hits;
-
-ALTER TABLE bioinf.kleborate
-ADD COLUMN truncated_resistance_hits TEXT;
-ALTER TABLE bioinf.kleborate
-ADD COLUMN flq_mutations TEXT;
+-- A rename
+ALTER TABLE bioinf.kleborate RENAME COLUMN spurious_virulence_hits TO spurious_resistance_hits;
+-- Adds columns
+ALTER TABLE bioinf.kleborate ADD COLUMN truncated_resistance_hits TEXT;
+ALTER TABLE bioinf.kleborate ADD COLUMN flq_mutations             TEXT;
