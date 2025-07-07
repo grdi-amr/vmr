963f170318599704062ddebabcfc88f3e3bc9e74
ejurga <ejacc@protonmail.com>
2025-07-07 15:13:44 -0400

    Adds version insert

--- src/migrations/v1.19_gw.sql
+++ src/migrations/v1.19_gw.sql
@@ -1,3 +1,7 @@
+
+INSERT INTO db_versions (major_release, minor_release, script_name, grdi_template_version, date_applied, note)
+   VALUES (2,7,'v2.07.sql', 'v14.5.4', CURRENT_DATE, 'Adds MASH table to bioinf');
+
 CREATE TABLE bioinf.refseq_masher (
     id int4 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     sequencing_id int4 REFERENCES sequencing(id),
