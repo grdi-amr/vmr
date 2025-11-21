5a1481e81c59b8f36377aa2b4b2076673d304a69
ejurga <ejacc@protonmail.com>
2025-11-21 12:53:17 -0500

    fixes formatting error

--- src/migrations/v2.12.sql
+++ src/migrations/v2.12.sql
@@ -1,5 +1,5 @@
 INSERT INTO db_versions (major_release, minor_release, script_name, grdi_template_version, date_applied, note)
-   VALUES (2,12,'v2.12.sql', 'v14.5.4', CURRENT_DATE, 'Altering values in columns from float/numeric to text since they can have multiple values separated to ";"'
+   VALUES (2,12,'v2.12.sql', 'v14.5.4', CURRENT_DATE, 'Altering values in columns from float/numeric to text since they can have multiple values separated to ";"');
 
 ALTER TABLE bioinf.ecoli_serotyping
     ALTER COLUMN pathotype_counts             TYPE TEXT,
