af8ca764273211a8cf3845a7339e65265c3c3cca
ejurga <ejacc@protonmail.com>
2026-02-06 16:34:09 -0500

    adds version increment

--- src/migrations/v2.14.sql
+++ src/migrations/v2.14.sql
@@ -1,3 +1,6 @@
+INSERT INTO db_versions (major_release, minor_release, script_name, grdi_template_version, date_applied, note)
+   VALUES (2,14,'v2.14.sql', 'v14.5.4', CURRENT_DATE, 'Adds bacment table to bioinf schema');
+
 CREATE TABLE bioinf.bacmet (
     id int4 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     sequencing_id int4 REFERENCES sequencing(id),
