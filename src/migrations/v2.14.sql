27e13efde4cee1ab3812ea911cf995bb03ccbfb3
ejurga <ejacc@protonmail.com>
2026-02-06 16:35:38 -0500

    linting

--- src/migrations/v2.14.sql
+++ src/migrations/v2.14.sql
@@ -4,22 +4,20 @@ INSERT INTO db_versions (major_release, minor_release, script_name, grdi_templat
 CREATE TABLE bioinf.bacmet (
     id int4 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     sequencing_id int4 REFERENCES sequencing(id),
-    file          TEXT,
-    sequence           TEXT,
-    start_pos           INTEGER NOT NULL,
-    end_pos             INTEGER NOT NULL,
-    strand              CHAR(1) CHECK (strand IN ('+', '-')),
-    gene             TEXT,         
-        -- Alignment metrics
-    coverage        TEXT,          
-    coverage_map        TEXT,
-    gaps                TEXT,
-    pct_coverage        FLOAT,
-    pct_identity        FLOAT,
-
+    file              TEXT,
+    sequence          TEXT,
+    start_pos         INTEGER NOT NULL,
+    end_pos           INTEGER NOT NULL,
+    strand            CHAR(1) CHECK (strand IN ('+', '-')),
+    gene              TEXT
+    -- Alignment metrics
+    coverage          TEXT,
+    coverage_map      TEXT,
+    gaps              TEXT,
+    pct_coverage      FLOAT,
+    pct_identity      FLOAT,
     -- BacMet-specific
-    database       TEXT,
-    prediction_status   TEXT NOT NULL
-        CHECK (prediction_status IN ('confirmed', 'predicted')),
-    resistance    TEXT,          
+    database          TEXT,
+    prediction_status TEXT NOT NULL CHECK (prediction_status IN ('confirmed', 'predicted')),
+    resistance        TEXT,
 );
