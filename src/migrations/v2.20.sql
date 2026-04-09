cd0f45710775e570c8c3e98265f786b9664af025
ejurga <ejacc@protonmail.com>
2026-04-09 18:46:37 -0400

    adds update table insert

--- src/migrations/v2.20.sql
+++ src/migrations/v2.20.sql
@@ -1,3 +1,6 @@
+INSERT INTO db_versions (major_release, minor_release, script_name, grdi_template_version, date_applied, note)
+   VALUES (2,20,'v2.20.sql', 'v14.5.4', CURRENT_DATE, 'Makes bioinf.qc_fastp columns bases_before and bases_after type bigint');
+
 ALTER TABLE bioinf.qc_fastp
     ALTER COLUMN bases_before TYPE bigint,
     ALTER COLUMN bases_after TYPE bigint;
