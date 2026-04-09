c70b12d1f0d831622d8cb6eb88c3a31a994009a4
ejurga <ejacc@protonmail.com>
2026-04-09 15:36:32 -0400

    Adds version table update

--- src/migrations/v2.19.sql
+++ src/migrations/v2.19.sql
@@ -1,1 +1,4 @@
+INSERT INTO db_versions (major_release, minor_release, script_name, grdi_template_version, date_applied, note)
+   VALUES (2,19,'v2.19.sql', 'v14.5.4', CURRENT_DATE, 'Makes bioinf.qc_raw_read_quality.qual_sum to be BIGINT');
+
 ALTER TABLE bioinf.qc_raw_read_quality ALTER COLUMN qual_sum TYPE BIGINT;
